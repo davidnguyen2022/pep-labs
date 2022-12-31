c6e276651a2af63638158a757fd23f3a91cebaef
Khoa Nguyen <davidnguyen08262000@gmail.com>
2022-12-31 09:00:48 +0000

    Saved progress on IfStatement 2022-12-31 09:00:48 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -16,7 +16,13 @@ public class IfStatement {
      * @param y second number to be compared.
      * @return x if bool is true. if bool is false, return y.
      */
-    public int ifExample(boolean bool, int x, int y){
-        return 0;
+    public boolean ifExample(boolean bool, int x, int y){
+        if(x<y) {
+        return x;
+        }
+        else{
+        return y;
+        }
     }
+    
 }
