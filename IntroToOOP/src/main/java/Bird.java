64a3a8e442e8efafef3a69af8216956e16b9a930
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-01-03 07:18:01 +0000

    Saved progress on IntroToOOP 2023-01-03 07:18:01 +0000

--- IntroToOOP/src/main/java/Bird.java
+++ IntroToOOP/src/main/java/Bird.java
@@ -38,6 +38,7 @@ public class Bird {
      * No need to change anything here, for now.
      */
     public Bird(){
+        
 
     }
     /**
@@ -45,7 +46,12 @@ public class Bird {
      *  * greater than or equal to 3.
      */
     public String sound(){
-        return "";
+        if (ageMonths < 3) {
+        return "Tweet";
+        }
+        else {
+            return "Chirp";
+        }
     }
 
 }
