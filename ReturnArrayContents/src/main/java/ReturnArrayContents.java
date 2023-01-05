f184f331202cf23625e2adb565b68e8e411ce563
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-01-05 21:56:57 +0000

    Saved progress on ReturnArrayContents 2023-01-05 21:56:57 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -28,9 +28,10 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
+        String buildingString = "";
         for (int i = 0; i < nums.length; i++) {
-  
+            buildingString = buildingString + nums[i] + " ";
         }
-        return nums[i] + "";
+        return buildingString;
     }
 }
