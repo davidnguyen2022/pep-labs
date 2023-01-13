e9b9ee749f59d88d6c37a4c4cc9cdea2b7198651
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-01-13 08:00:07 +0000

    Saved progress on Lists 2023-01-13 08:00:07 +0000

--- Lists/src/main/java/ListExercise.java
+++ Lists/src/main/java/ListExercise.java
@@ -30,7 +30,8 @@ public class ListExercise {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+        List<Integer> object =new ArrayList<Integer>();
+        return object;
     }
 
     /**
