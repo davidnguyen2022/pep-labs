5c36a2b1b818881eb3d0c0d98e7a8ab6f32c9aa5
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-01-18 23:45:07 +0000

    Saved progress on Insert 2023-01-18 23:45:07 +0000

---
+++ Insert/problem1.sql
@@ -0,0 +1,4 @@
+INSERT INTO SONG (title,artist) values ("Let it be", "Beatles");
+ INSERT INTO SONG (title,artist) values ("Hotel California", "Eagles");
+ INSERT INTO SONG (title,artist) values ("Kashmir", "Led Zeppelin");
+
