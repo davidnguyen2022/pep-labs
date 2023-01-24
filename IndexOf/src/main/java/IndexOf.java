8e59a0dabb5a0ebec801852eb615e23a47ef3bba
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-01-24 13:15:53 +0000

    Saved progress on IndexOf 2023-01-24 13:15:53 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -11,6 +11,13 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
+        
+        int len = arr.length;
+        int i = 0;
+        while (i < len) {
+            if (arr[i] == n) return i;
+            else i=i+1;
+        }
         return -1;
     }
 }
