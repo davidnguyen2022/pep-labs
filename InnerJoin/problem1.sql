e040c5ffbba8be7a746034c932b3d3cf440612fa
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-01-28 12:38:45 +0000

    Saved progress on InnerJoin 2023-01-28 12:38:45 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,4 +1,4 @@
-SELECT * FROM class INNER JOIN student 
+SELECT id, teacher_name, student_name, class.title FROM class INNER JOIN student 
 ON class.class = student.class 
 WHERE class.id = student.id, class.teacher_name = "Ms. Lovelace" = student.student_name, 
 class.class_title = student.student_title;
