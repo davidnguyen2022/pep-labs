1f7f5998517f197a0599e45f2d3a56dca8f9c483
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-01-28 08:04:11 +0000

    Saved progress on CreatingEndpoints 2023-01-28 08:04:11 +0000

--- CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
+++ CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
@@ -3,22 +3,28 @@ package com.revature;
 import io.javalin.Javalin;
 
 public class JavalinSingleton {
+    
+
+
 
     
     /**
      * Lab: Create an HTTP GET endpoint at the url: http://localhost:9000/hello that will return the string "Hello
      * World" in the response.
-     * 
+     
      * Note: Please refer to the "CreatingEndpoints.MD" file for more assistance if needed.
      */
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
         
+        
         // HTTP GET REQUEST to http://localhost:9000/get-request
-        app.get("/get-request", ctx -> {
+        app.get("/hello", ctx -> {
             //logic to be executed when this endpoint is hit
-            ctx.result("get request endpoint hit!");
+           // ctx.result("get request endpoint hit!");
+           ctx.result("Hello World");
         });
+        
 
         return app;
     }
