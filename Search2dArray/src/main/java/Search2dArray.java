f50d9c0102d1e3045dfca3fb363491b8e87e2380
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-02-07 08:22:29 +0000

    Saved progress on Search2dArray 2023-02-07 08:22:29 +0000

--- Search2dArray/src/main/java/Search2dArray.java
+++ Search2dArray/src/main/java/Search2dArray.java
@@ -10,6 +10,10 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
-        return false;
+        for (int i = 0; i<nums.length; i++)
+        {
+            return true;
     }
+    return false;
+}
 }
