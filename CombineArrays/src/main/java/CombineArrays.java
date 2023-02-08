c4345601b5c696c2e0b767570453475687091ce5
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-02-08 09:14:14 +0000

    Saved progress on CombineArrays 2023-02-08 09:14:14 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -12,17 +12,14 @@ public class CombineArrays {
         int[]arr3 = new int [arr1.length + arr2.length];
         
         for (int i = 0; i<arr1.length; i++) {
-            for (int j = 0; j<arr2.length; j++) {
+            arr3[i] += arr1[i];
+        }
+            for (int i = 0; i<arr2.length; i++) {
+                arr3[arr1.length + i] += arr2[i];
 
             }
-            
-        }
-        return arr3;
+            return arr3;
         
-        
-
-           
-            
         
     } 
 }
