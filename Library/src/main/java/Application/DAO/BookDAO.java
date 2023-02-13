ca9738130c580e310d36cda680ddb5d9481939da
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-02-13 21:08:05 +0000

    Saved progress on Library 2023-02-13 21:08:04 +0000

--- Library/src/main/java/Application/DAO/BookDAO.java
+++ Library/src/main/java/Application/DAO/BookDAO.java
@@ -88,12 +88,16 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "INSERT INTO book (name) values (?);" ;
+            String sql = "INSERT INTO book (isbn, author_id, title, copies_available) values (?,?,?,?);" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setString and setInt methods here.
-            preparedStatement.setString(1, book.getTitle());
-            //preparedStatement.setInt();
+            preparedStatement.setInt(1, book.getIsbn());
+            preparedStatement.setInt(2, book.getAuthor_id());
+            
+            preparedStatement.setString(3, book.getTitle());
+            preparedStatement.setInt(4, book.getCopies_available());
+            
 
             preparedStatement.executeUpdate();
             return book;
@@ -112,10 +116,11 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             
-            String sql = "change me";
+            String sql = "SELECT * from book where copies_available > 0";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
+            
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
--- Library/src/main/java/Application/Service/BookService.java
+++ Library/src/main/java/Application/Service/BookService.java
@@ -51,15 +51,19 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
-
-        return bookDAO.insertBook(book);
+        if (bookDAO.getBookByIsbn(book.getIsbn()) == null){
+            return bookDAO.insertBook(book);
+        }
+        return null;
+        
+        
     }
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return bookDAO.getAllAvailableBooks();
+        return bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
