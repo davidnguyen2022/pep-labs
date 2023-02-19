b200df53e040723a42f5bf1fce199cbe26890f4f
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-02-19 07:58:26 +0000

    Saved progress on ParentObjectClassMethods 2023-02-19 07:58:26 +0000

--- ParentObjectClassMethods/src/main/java/SampleClass.java
+++ ParentObjectClassMethods/src/main/java/SampleClass.java
@@ -56,9 +56,11 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    a.equals(b){};
 
 
     //    implement a custom .toString(){} method here.
+    a.toString(b){};
 
     
 }
