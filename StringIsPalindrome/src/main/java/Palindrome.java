69e39d0bd44ce241b90ea24033b681990a90b117
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-02-23 11:18:10 +0000

    Saved progress on StringIsPalindrome 2023-02-23 11:18:10 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,6 +12,18 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        String reverse = "";
+ 
+        boolean answer = false;
+ 
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+ 
+        // Checking if both the strings are equal
+        if (str.equals(reverse)) {
+            answer = true;
+        }
+        return answer;
     }
 }
