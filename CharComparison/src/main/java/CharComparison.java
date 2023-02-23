edd761ced86aed279a1490efd968c27f7ae9f169
Khoa Nguyen <davidnguyen08262000@gmail.com>
2023-02-23 06:37:51 +0000

    Saved progress on CharComparison 2023-02-23 06:37:51 +0000

--- CharComparison/src/main/java/CharComparison.java
+++ CharComparison/src/main/java/CharComparison.java
@@ -11,6 +11,16 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
-        return 0;
+        var result = 0;
+        for(int i = 0; i < b.length;) {
+            if((a[i] < b[i]) || (a[i] > b[i])) {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        }
+        return result;
+
     }
 }
